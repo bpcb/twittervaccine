311dc4a92c7741d389a6018b2286a85148f7eadc
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-03 15:16:07 -0700

    Delete sample commit

--- echonest.py
+++
@@ -1,9 +0,0 @@
-from pyechonest import config, artist
-
-config.ECHO_NEST_API_KEY = 'OT96HEX0YVCGJEMZK'
-
-washed_out = artist.Artist('classixx')
-print washed_out.style
-
-chillwave = artist.search(style='chillwave')
-print chillwave
