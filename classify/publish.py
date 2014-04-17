468a3a734dd667a7f94ef35d219a7173ad57db98
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-17 18:39:43 +0000

    module to publish sentiment analysis results

---
+++ classify/publish.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""Publish sentiment data to the database."""
+
+import MySQLdb
+import git_rev
+
+# mapping from algorithm name to code; this really should be stored in the database
+ALGORITHMS = { 'vsps': 1, 'afinn111' : 2, 'test': 999}
+
+def get_database_connection():
+    conn = MySQLdb.connect(host='localhost', user='root', db='vaccine')
+    return conn
+
+CREATE_SQL = """
+CREATE TABLE IF NOT EXISTS sentiment_score(
+  tweet_id BIGINT, algorithm SMALLINT, revision VARCHAR(20), result FLOAT,
+  PRIMARY KEY (tweet_id, algorithm, revision));
+"""
+
+INSERT_SQL = """
+INSERT INTO sentiment_score(tweet_id, algorithm, revision, result)
+VALUES """
+
+def publish_sentiment(algorithm, tweets):
+    """Store a list of sentiment analysis results into the data base.
+
+    :param algorithm: The algorithm used to classify tweets; can be a string or
+    or an integer.
+    :param tweets: An iterable list of (tweet_id, sentiment_score) tuples.
+    """
+
+    rev = git_rev.git_current_revision()
+    if isinstance(algorithm, str):
+        algo = ALGORITHMS[algorithm]
+    else:
+        algo = algorithm
+
+    conn = get_database_connection()
+    cursor = conn.cursor()
+    cursor.execute(CREATE_SQL)
+
+    val_strs = ['(%d, %f)' % (_id, score) for (_id, score) in tweets]
+    sql = INSERT_SQL + ','.join(val_strs)
+    cursor.execute(sql)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    # create some dummy test data
+    tweets = [(1, -.9), (2, -.3), (3, .7), (4, 0.0), (5, 1.1)]
+    publish_sentiment('test', tweets)
+
