f2e924ee18465409ac75de883f4943c66b4d7c68
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-04-17 13:56:03 -0700

    Testing using Yahoo PlaceFinder

--- geocode/geopy_test.py
+++ geocode/geopy_test.py
@@ -2,6 +2,7 @@ from geopy import geocoders, point
 import csv
 from time import sleep
 import re
+import yql
 
 #These are identifiers that sometimes preprend lat, long strings
 REVERSE_IDENTIFIERS = ['\xc3\x9cT:', 'iPhone:']
@@ -48,41 +49,57 @@ def reverse_geocode_location(location):
 	return None
 	
 def main():
-	infile = csv.reader(open(file_name, 'r'), delimiter=',')
-	outfile = csv.writer(open(out_file_name, 'w'), delimiter=',')
+	# infile = csv.reader(open(file_name, 'r'), delimiter=',')
+	# outfile = csv.writer(open(out_file_name, 'w'), delimiter=',')
 
-	geolocator = geocoders.GoogleV3()
+	consumer_key = 'dj0yJmk9NWM0azdWc0xWMTZJJmQ9WVdrOU5qTkJUbTlxTjJjbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wMQ--'
+	consumer_key_secret = '737f0bc32079f654abf72d125d103dc6f900fa79'
 	
-	location = ''
-	address = None
-	latitude = None
-	longitude = None
+	geolocator = geocoders.YahooPlaceFinder(consumer_key = consumer_key, consumer_secret = consumer_key_secret)
+	
+	location = 'Washington DC'
+	raw_output = geolocator.geocode(location)
+	print raw_output
+	
+	# location = ''
+	# address = None
+	# latitude = None
+	# longitude = None
 
-	for line in infile:
-		user = line[0]
-		location = line[1].replace('\n', '')
-		print location
+	# for line in infile:
+		# user = line[0]
+		# location = line[1].replace('\n', '')
 		
-		if location is not '':
-			gps = False
-			for id in REVERSE_IDENTIFIERS:
-				if re.search(id, location):
-					location = strip_reverse_identifiers(location)
-					coordinates = point.Point(latitude = float(location[-19:-10]), longitude = float(location[-10:]))
-					address = geolocator.reverse(coordinates) 
-					outfile.writerow([user, location, address, latitude, longitude])
-					gps = True
-			if gps == False:	
-				try:
-					location = remove_stop_words(location)
-					address, (latitude, longitude) = geolocator.geocode("%s" % (location))
-					outfile.writerow([user, location, address, latitude, longitude])
-				except:
-					outfile.writerow([user, location])
-		else:
-			outfile.writerow([user])
+		# print location
+		# raw_output = geolocator.geocode(location, raw = True)
+		# print raw_output
+
+		# print location
+		
+		# if location is not '':
+			# gps = False
+			# for id in REVERSE_IDENTIFIERS:
+				# if re.search(id, location):
+					# location = strip_reverse_identifiers(location)
+					# coordinates = point.Point(latitude = float(location[-19:-10]), longitude = float(location[-10:]))
+					# address = geolocator.reverse(coordinates) 
+					# outfile.writerow([user, location, address, latitude, longitude])
+					# gps = True
+			# if gps == False:	
+				# try:
+					# location = remove_stop_words(location)
+					# address, (latitude, longitude) = geolocator.geocode("%s" % (location))
+					# outfile.writerow([user, location, address, latitude, longitude])
+				# except:
+					# outfile.writerow([user, location])
+		# else:
+			# outfile.writerow([user])
 			
-		sleep(1)
+		# sleep(1)
+		
+		# y = yql.Public()
+		# query = 'SELECT * FROM geo.placefinder WHERE text = @text AND api_key = @consumer_key';
+		# y.execute(query, {"text": "Washington DC", "consumer_key": consumer_key})
 		
 if __name__ == '__main__':
 	main()
