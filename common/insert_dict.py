79bb9aa05893b453867b0e26ae94df8dbdca1ce7
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-24 22:58:26 +0000

    ensure deterministic dictionary traversal

--- common/insert_dict.py
+++ common/insert_dict.py
@@ -9,17 +9,21 @@ from db import get_database_connection
 
 record = {'col1': 7, 'col2': "pizza pizza", 'col3': True}
 
+tups = [(key, val) for key, val in record.iteritems()]
+keys = [key for key, val in tups]
+vals = [val for key, val in tups]
+
 conn = get_database_connection()
 cursor = conn.cursor()
 
-num_cols = len(record)
-key_str = ','.join(record.keys())
+num_cols = len(keys)
+key_str = ','.join(keys)
 val_str = ','.join(['%s'] * num_cols)
 
 qry = "INSERT INTO vaccine.dummy (%s) VALUES (%s)" % (key_str, val_str)
 print qry
 
-cursor.execute(qry, record.values())
+cursor.execute(qry, vals)
 
 conn.commit()
 cursor.close()
