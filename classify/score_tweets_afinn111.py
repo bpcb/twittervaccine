7860f410eba1d6701b002044c9e9152c79555ac5
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-29 22:28:20 +0000

    Fix computation of afinn_111 sentiment scores

--- classify/score_tweets_afinn111.py
+++ classify/score_tweets_afinn111.py
@@ -6,7 +6,7 @@ import extract
 import score
 import publish
 
-scorer = score.SentimentScorer.from_vaccine_phrases()
+scorer = score.SentimentScorer.from_afinn_111()
 results = [(_id, scorer.get_document_score(text, normalize=False))
            for (_id, text) in extract.extract_text()]
 publish.publish_sentiment('afinn111', results)
