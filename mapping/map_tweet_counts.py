270b9bae79bc11324132775185b054428a248d01
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-05-13 13:24:44 -0700

    Mapping average sentiment scores by county

--- mapping/map_tweet_counts.py
+++ mapping/map_tweet_counts.py
@@ -73,34 +73,54 @@ def getDict ( county, shapefile ):
 
     return countyDict
 	
+def get_colors(score, min, max, palette):
+	pass
+	
+def county_means(county_results):
+	means = {key: np.mean(val) for key, val in county_results.iteritems()}
+	return means
+	
+def generate_webpage():
+	# Read the ShapeFile
+	dat = shapefile.Reader("./shapefile/USCounties.shp")
+
+	# Create a list of county tuples of (County, State, Color)
+	counties = [(i[0], i[1], "#FFFEFF") for i in dat.iterRecords()]
+
+	# Create the Plot
+	from bokeh.plotting import *
+	output_file("us_counties.html")
+
+	TOOLS="pan,wheel_zoom,box_zoom,reset,previewsave"
+	figure(title="Vaccination Tweets by County", tools=TOOLS, plot_width=900, plot_height=800)
+	
+	hold()
+	
+	count = 0
+	for county in counties:
+		data = getDict(county, dat)
+		patches(data[(county[0], county[1])]['lat_list'], data[(county[0], county[1])]['lng_list'], \
+				fill_color = county[2], line_color = "black")
+
+	show()
+	
 def main():
-	# # Read the ShapeFile
-	# dat = shapefile.Reader("./shapefile/USCounties.shp")
-
-	# # Create a list of county tuples of (County, State, Color)
-	# counties = [(i[0], i[1], "#FFFEFF") for i in dat.iterRecords()]
-
-	# # Create the Plot
-
-	# from bokeh.plotting import *
-	# output_file("us_counties.html")
-
-	# TOOLS="pan,wheel_zoom,box_zoom,reset,previewsave"
-	# figure(title="Vaccination Tweets by County", tools=TOOLS, plot_width=900, plot_height=800)
-	
-	# hold()
-	
-	# count = 0
-	# for county in counties:
-		# data = getDict(county, dat)
-		# patches(data[(county[0], county[1])]['lat_list'], data[(county[0], county[1])]['lng_list'], \
-				# fill_color = county[2], line_color = "black")
-
-	# show()
-	
 	d = anova.average_user_scores()
 	d2 = anova.county_results(d)
+	average_scores = county_means(d2)
 	
-.strip('County', 'City').strip(' ')
+	min_score = min(average_scores.values())
+	max_score = max(average_scores.values())
+	palette = ["#FFF7EC", "#FEE8C8", "#FDD49E", "#FDBB84", "#FC8D59", "#EF6548", "#D7301F", "#B30000" "#7F0000"]
+	
+	for score in average_scores:
+		get_colors(score, min, max, palette)
+		
+	# generate_webpage()
+	
+# County names from shape file have "County" and "City" at the end of them.
+# Use this syntax to strip them once appropriate.
+# .strip('County', 'City').strip(' ')
 
 main()
+
