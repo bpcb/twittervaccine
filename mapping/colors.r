bf67a4b382ed262b2835d74e9b7b511eb66a07aa
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-05-13 11:54:37 -0700

    Use ColorBrewer to generate colors for map

---
+++ mapping/colors.r
@@ -0,0 +1,5 @@
+library(RColorBrewer)
+
+base_colors <- brewer.pal(9, "OrRd")
+# Add this line to interpolate between these 9 colors to generate 100 colors.
+# interpolated_colors <- colorRampPalette(colors)(100)
