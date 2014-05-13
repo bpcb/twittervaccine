49c5d1af289f4ef6116e638868d40a3898bb3479
Andrew Whitaker <whitaker@cs.washington.edu>
2014-05-13 23:47:22 +0000

    refactor evaluation code

--- classify/eval_vsps.py
+++ classify/eval_vsps.py
@@ -1,33 +1,8 @@
 #!/usr/bin/env python
 
-"""Evaluate the VSPS sentiment scores compared to labled training data."""
-
-import extract
 import score
-import publish
+import evaluate
 
 scorer = score.SentimentScorer.from_vaccine_phrases()
-
-# confusion matrix: label, result => count
-results = {(x,y) : 0 for x in ['-', 'X'] for y in ['-', 'X']}
-
-for _id, label, text in extract.extract_classified_tweets():
-    score = scorer.get_document_score(text, normalize=False)
-    if score < 0:
-        result = '-'
-    else:
-        result = 'X'
-    results[(label, result)] += 1
-
-print results
-
-tp = results[('-', '-')]
-tn = results[('X', 'X')]
-fp = results[('X', '-')]
-fn = results[('-', 'X')]
-
-precision = float(tp) / (tp + fp)
-recall = float(tp) / (tp + fn)
-accuracy = (float(tp) + float(tn)) / (tp + tn + fn + fp)
-
-print precision, recall, accuracy
+results = evaluate.evaluate(scorer)
+evaluate.display_results(results)
