16f26f37a8afd92107322cabdb118f2d3aa3563f
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-05-15 16:57:48 -0700

    Add color gradient based on average county score

--- mapping/map_tweet_counts.py
+++ mapping/map_tweet_counts.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import shapefile
 import sys
+import math
+import bokeh.plotting as bp
 
 # Hack: append common/ to sys.path
 sys.path.append("../common")
@@ -73,8 +75,11 @@ def getDict ( county, shapefile ):
 
     return countyDict
 	
-def get_colors(score, min, max, palette):
-	pass
+def get_color(score, min_score, palette):
+	index = int(math.floor((score / min_score) * len(palette)))
+	if score == min_score:
+		index = 8
+	return palette[index]
 	
 def county_means(county_results):
 	means = {key: np.mean(val) for key, val in county_results.iteritems()}
@@ -83,44 +88,48 @@ def county_means(county_results):
 def generate_webpage():
 	# Read the ShapeFile
 	dat = shapefile.Reader("./shapefile/USCounties.shp")
-
-	# Create a list of county tuples of (County, State, Color)
-	counties = [(i[0], i[1], "#FFFEFF") for i in dat.iterRecords()]
-
-	# Create the Plot
-	from bokeh.plotting import *
-	output_file("us_counties.html")
-
-	TOOLS="pan,wheel_zoom,box_zoom,reset,previewsave"
-	figure(title="Vaccination Tweets by County", tools=TOOLS, plot_width=900, plot_height=800)
 	
-	hold()
-	
-	count = 0
-	for county in counties:
-		data = getDict(county, dat)
-		patches(data[(county[0], county[1])]['lat_list'], data[(county[0], county[1])]['lng_list'], \
-				fill_color = county[2], line_color = "black")
-
-	show()
-	
-def main():
 	d = anova.average_user_scores()
 	d2 = anova.county_results(d)
 	average_scores = county_means(d2)
 	
-	min_score = min(average_scores.values())
-	max_score = max(average_scores.values())
-	palette = ["#FFF7EC", "#FEE8C8", "#FDD49E", "#FDBB84", "#FC8D59", "#EF6548", "#D7301F", "#B30000" "#7F0000"]
+	a2 = dict()
+	for key in average_scores.keys():
+		new_key = key[0].strip('City').strip('County').strip(' ')
+		a2[(new_key, key[1])] = average_scores[key]
 	
-	for score in average_scores:
-		get_colors(score, min, max, palette)
-		
-	# generate_webpage()
+	min_score = min(a2.values())
+	palette = ["#FFF7EC", "#FEE8C8", "#FDD49E", "#FDBB84", "#FC8D59", "#EF6548", "#D7301F", "#B30000", "#7F0000"]
 	
-# County names from shape file have "County" and "City" at the end of them.
-# Use this syntax to strip them once appropriate.
-# .strip('County', 'City').strip(' ')
+	colors = dict()
+	for county in a2:
+		colors[county] = get_color(a2[county], min_score, palette)
+
+	# Create a list of county tuples of (County, State)
+	counties = [(i[0], i[1]) for i in dat.iterRecords()]
+
+	# Create the Plot
+	bp.output_file("us_counties.html")
+
+	TOOLS="pan,wheel_zoom,box_zoom,reset,previewsave"
+	bp.figure(title="Vaccination Tweets by County", tools=TOOLS, plot_width=900, plot_height=800)
+	
+	bp.hold()
+	
+	count = 0
+	for county in counties:
+		data = getDict(county, dat)
+		if county in colors:
+			fill = colors[county]
+		else:
+			fill = "EEE9E9"
+		bp.patches(data[(county[0], county[1])]['lat_list'], data[(county[0], county[1])]['lng_list'], \
+				fill_color = fill, line_color = "black")
+
+	show()
+	
+def main():
+	generate_webpage()
 
 main()
 
