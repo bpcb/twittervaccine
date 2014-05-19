7510258af550942bcd97afc99bda53288099365e
Andrew Whitaker <whitaker@cs.washington.edu>
2014-05-19 11:53:46 -0700

    Train and test multinomial naive bayes

--- classify/sk.py
+++ classify/sk.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import numpy as np
+
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.cross_validation import train_test_split
 
 import extract
 
@@ -16,6 +20,12 @@ def convert_labels_to_binary(Y, one_label_list):
 
     return X
 
+def create_classifier():
+    cv = CountVectorizer()
+    clf = MultinomialNB()
+    pipeline = Pipeline([('vect', cv), ('clf', clf)])
+    return pipeline
+
 if __name__ == "__main__":
     results = list(extract.extract_classified_tweets(1000))
     _ids, votes, texts = zip(*results)
@@ -24,4 +34,9 @@ if __name__ == "__main__":
     labels = np.asarray(votes)
 
     labels = convert_labels_to_binary(labels, ['-'])
-    print labels
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        tweets, labels, test_size=0.2, random_state=0)
+    clf = create_classifier()
+    clf.fit(X_train, y_train)
+    print clf.score(X_test, y_test)
