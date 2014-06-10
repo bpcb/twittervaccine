b7d4140ea149713edab64a536fcbdb7801624616
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-06-10 15:43:39 -0700

    Compare several sentiment algorithms to H1N1 vaccination data

--- analyze/rates_sentiment_compare.py
+++ analyze/rates_sentiment_compare.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 vac_rates = pd.read_csv('./vaccination_rates_2010.csv')
@@ -7,7 +8,18 @@ sentiment = pd.read_csv('./state_sentiment_scores.csv')
 combined = pd.merge(h1n1_vac_rates, sentiment, on = 'state')
 
 correlation = combined.corr()
-print correlation['score']
+print "AFINN:", correlation['score_afinn']
+print "Bayes:", correlation['score_bayes']
+print "Logistic:", correlation['score_logistic']
 
-plt.scatter(combined['score'], combined['6mo_17yr'])
-plt.show()
+x = np.arange(0.80, 0.86, 0.01)
+m, b = np.polyfit(combined['score_logistic'], combined['init_target_groups'], 1)
+
+plt.scatter(combined['score_logistic'], combined['init_target_groups'])
+plt.grid()
+plt.plot(x, m*x + b, '-', color = 'red', linewidth = 2)
+plt.xlabel('Average user sentiment score by state')
+plt.ylabel('H1N1 vaccination rate in 2010 in high risk groups (%)')
+plt.rcParams['xtick.major.pad'] = 8
+plt.rcParams['ytick.major.pad'] = 8
+plt.savefig('./logistic_sentiment_vacc_rate.png')Tzbh64Y5
