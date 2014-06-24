185623572806e3134b395bfb914847255ad659c9
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-06-23 17:50:28 -0700

    Making geocoding code more flexible to work with 2014 data

--- geocode/extract.py
+++ geocode/extract.py
@@ -16,32 +16,49 @@ from geocode import *
 from db import get_database_connection
 from insert_dict import *
 
-def extract_tweeters():
+def extract_tweeters(table):
 	"""
 	Extract all users with geographic information.
 	"""
+	
+	if table != 'users_2014' and table != 'tweeter_tweeter':
+		print 'extract_tweeters function only works with tables of users (users_2014 and tweeter_tweeter)'
+		break
 	
 	users = {}
 	
 	conn = get_database_connection(port = 2001)
 	cursor = conn.cursor()
-	cursor.execute('SELECT user_name, id, location_string FROM tweeter_tweeter')
 	
-	for user_name, id, location_string in cursor.fetchall():
-		if location_string is not None:
-			user = users.get(id, Tweeter(id))
-			user.location = location_string
-			user.user_name = user_name
-
-						users[id] = user
+	# This execute statement depends on which table you are trying to extract users from.
+	# If 2009 dataset, use 'SELECT user_name, id, location_string FROM tweeter_tweeter'
+	if table == 'users_2014':
+		cursor.execute('SELECT user_id, location FROM users_2014')
+	elif table == 'tweeter_tweeter':
+		cursor.execute('SELECT user_name, id, location_string FROM tweeter_tweeter')
+	
+	if table == 'tweeter_tweeter':
+		for user_name, id, location_string in cursor.fetchall():
+			if location_string is not None:
+				user = users.get(id, Tweeter(id))
+				user.location = location_string
+				user.user_name = user_name
+				users[id] = user
+	elif table == 'users_2014':
+		for user_id, location in cursor.fetchall():
+			if location is not None:
+				user = users.get(user_id, Tweeter(user_id))
+				user.location = location
 		
-		cursor.close()
-		conn.close()
+	cursor.close()
+	conn.close()
 
-		return users.values()
+	return users.values()
 
 if __name__ == '__main__':
-	users = extract_tweeters()
+	table = 'users_2014'
+	
+	users = extract_tweeters(table)
 
 	for user in users:
 		try:
