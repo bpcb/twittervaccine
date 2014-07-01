60240ef34f4efa225439cd52df03bc133903852c
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-07-01 10:33:00 -0700

    Changes to queries to get average sentiment by geography for 2014 data

--- queries/anova.py
+++ queries/anova.py
@@ -10,15 +10,39 @@ import collections
 def average_user_scores():
 	"""
 	Queries SQL database to return an object with average score
-	by user with their associated county and state locations.
+	by user with their associated city and state locations.
 	"""
 	
 	query = """
 	-- emit average user score; note that user id defines location
-	select L.id AS user_id, L.county, L.state, avg(result) AS user_avg_score
+	select L.id AS user_id, L.city, L.state, avg(result) AS user_avg_score
 	from tweets_tweet AS T, sentiment_score AS S, usa_user_locations AS L
-	where T.id=S.id AND T.tweeter_id=L.id AND algorithm='3' AND L.county is not NULL
-	group by L.id, L.county, L.state;
+	where T.id=S.id AND T.tweeter_id=L.id AND algorithm='5' AND L.city is not NULL
+	group by L.id, L.city, L.state;
+	"""
+
+	conn = pymysql.connect(host='localhost', port=2001, user='root', db='vaccine')
+	cursor = conn.cursor()
+	cursor.execute(query)
+
+	results = cursor.fetchall()
+	cursor.close()
+	conn.close()
+	
+	return results
+	
+def average_user_scores_2014():
+	"""
+	Queries SQL database to return an object with average score
+	by user with their associated city and state locations.
+	"""
+	
+	query = """
+	-- emit average user score; note that user id defines location
+	select  L.user_id, L.city, L.state, avg(result) AS user_avg_score
+	from tweets_2014 AS T, sentiment_score_2014 AS S, user_locations_2014 AS L
+	where T.tweet_id=S.tweet_id AND T.user_id=L.user_id AND algorithm='5' AND L.city is not NULL
+	group by L.user_id, L.city, L.state;
 	"""
 
 	conn = pymysql.connect(host='localhost', port=2001, user='root', db='vaccine')
@@ -31,24 +55,28 @@ def average_user_scores():
 	
 	return results
 
-def county_results(query_results):
+def city_results(query_results):
 	"""
-	Returns a dictionary with 2-tuple keys (county, state) 
+	Returns a dictionary with 2-tuple keys (city, state) 
 	associated with a list of average scores for users
-	in counties where there are at least 20 users.
+	in counties where there are at least 10 users.
 	"""
 	
 	d = collections.defaultdict(list)
-	for _id, county, state, avg in query_results:
-		d[(county, state)].append(avg)
+	for _id, city, state, avg in query_results:
+		d[(city, state)].append(avg)
 
 	# Filter out counties with less than 20 users.
-	d2 = {key: val for key, val in d.iteritems() if len(val) >= 20}
+	d2 = {key: val for key, val in d.iteritems() if len(val) >= 10}
 	
 	return d2
 
 d = average_user_scores()
-d2 = county_results(d)
+d2 = city_results(d)
+
+d_2014 = average_user_scores_2014()
+d2_2014 = city_results(d_2014)
+print len(d2_2014)
 
 # Run ANOVA over resulting 417 counties.
 
--- queries/county_mean_by_user.sql
+++ queries/county_mean_by_user.sql
@@ -3,6 +3,6 @@ select CONCAT(county, " ", state), avg(user_avg_score) AS avg_score FROM (
     -- emit average score per user
     select L.id AS user_id, L.county, L.state, avg(result) AS user_avg_score
     from tweets_tweet AS T, sentiment_score AS S, usa_user_locations AS L
-    WHERE T.id=S.id AND T.tweeter_id=L.id AND algorithm=3' AND L.county IS NOT NULL
+    WHERE T.id=S.id AND T.tweeter_id=L.id AND algorithm=5' AND L.county IS NOT NULL
     group by L.id, L.county, L.state) X
 group by county, state;
