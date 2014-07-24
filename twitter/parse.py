54cfc3b87c64d3669f587534c780cdb7b16bce87
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-07-24 14:21:16 -0700

    Updating script that parses 2014 tweets to deal with Unicode errors

--- twitter/parse.py
+++ twitter/parse.py
@@ -12,39 +12,25 @@ from insert_dict import insert_record
 Parse tweets as emitted by the twitter API.
 """
 
-user_errors = open('user_errors.txt', 'w')
-tweet_errors = open('tweet_errors.txt', 'w')
-
 
 for json_file in glob.glob('/mnt/tweets*.json'):
-	print json_file
-	with open(json_file) as f:
-		for line in f:
-			try:
-				obj = json.loads(line)
-			except ValueError:
-				continue
+    print json_file
+    with open(json_file) as f:
+        for line in f:
+            obj = json.loads(line)
+            print obj
 
-			tweet = dict()
-			user = dict()		
-		
-			tweet['tweet_id'] = obj['id']
-			tweet['user_id'] = obj['user']['id']
-			tweet['text'] = obj['text']
-			tweet['created_at'] = obj['created_at']
+            tweet = dict()
+            user = dict()        
+        
+            tweet['tweet_id'] = obj['id']
+            tweet['user_id'] = obj['user']['id']
+            tweet['text'] = obj['text']
+            tweet['created_at'] = obj['created_at']
 
-			user['user_id'] = obj['user']['id']
-			user['user_name'] = obj['user']['screen_name']
-			user['location'] = obj['user']['location']
-		
-			try:
-				insert_record(tweet, 'tweets_2014')
-			except UnicodeEncodeError:
-				tweet_errors.write(str(tweet['tweet_id']) + '\n')
-			try:
-				insert_record(user, 'users_2014')
-			except UnicodeEncodeError:
-				user_errors.write(str(user['user_id']) + '\n')
-
-user_errors.close()
-tweet_errors.close()
+            user['user_id'] = obj['user']['id']
+            user['user_name'] = obj['user']['screen_name']
+            user['location'] = obj['user']['location']
+        
+            insert_record(tweet, 'tweets_2014')
+            insert_record(user, 'users_2014')
