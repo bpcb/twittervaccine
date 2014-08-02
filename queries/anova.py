b11fc86f9255aa61eea95d482c37f74ede66de6d
benjamin-brooks <benjamin.p.brooks@gmail.com>
2014-08-01 17:05:43 -0700

    Updating comparison of 2009 and 2014 data

--- queries/anova.py
+++ queries/anova.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pymysql
 import scipy.stats
 import numpy as np
+import matplotlib.pyplot as plt
 import collections
 import sys
 
@@ -11,30 +12,30 @@ sys.path.append("../common")
 from db import get_database_connection
 
 # Use SQL to compute the average sentiment score per user. 
-# Drop state-only entries (for which the county is not known).
+# Drop state-only entries (for which the city is not known).
 
 # Query that emits average value by user for 2009 data.  
 query_2009 = """
-SELECT L.id AS user_id, L.county, L.state, avg(result) AS user_avg_score
+SELECT L.id AS user_id, L.city, L.state, avg(result) AS user_avg_score
 FROM tweets_tweet AS T, sentiment_score AS S, usa_user_locations AS L
-WHERE T.id=S.id AND T.tweeter_id=L.id AND algorithm='5' AND L.county IS NOT NULL AND L.country='United States'
-group by L.id, L.county, L.state;
+WHERE T.id=S.id AND T.tweeter_id=L.id AND algorithm='5' AND L.city IS NOT NULL AND L.country='United States'
+group by L.id, L.city, L.state;
 """
 
 # Query that instead emits the average value for users in 2014 data.
 query_2014 = """
 (
-SELECT L.user_id, L.county, L.state, AVG(result) AS user_avg_score
+SELECT L.user_id, L.city, L.state, AVG(result) AS user_avg_score
 FROM tweets_2014 AS T, sentiment_score_2014 AS S, user_locations_2014 AS L
-WHERE T.tweet_id=S.tweet_id AND T.user_id=L.user_id AND algorithm='5' AND L.county IS NOT NULL AND L.country='United States'
-GROUP BY L.user_id, L.county, L.state
+WHERE T.tweet_id=S.tweet_id AND T.user_id=L.user_id AND algorithm='5' AND L.city IS NOT NULL AND L.country='United States'
+GROUP BY L.user_id, L.city, L.state
 )
 """
 
 def average_user_scores(query):
     """
     Queries SQL database to return an object with average score
-    by user with their associated county and state locations.
+    by user with their associated city and state locations.
     """
 
     conn = get_database_connection(port = 2001)
@@ -47,16 +48,16 @@ def average_user_scores(query):
     
     return results
 
-def county_results(query_results):
+def city_results(query_results):
     """
-    Returns a dictionary with 2-tuple keys (county, state) 
+    Returns a dictionary with 2-tuple keys (city, state) 
     associated with a list of average scores for users
     in counties where there are at least 20 users.
     """
     
     d = collections.defaultdict(list)
-    for _id, county, state, avg in query_results:
-        d[(county, state)].append(avg)
+    for _id, city, state, avg in query_results:
+        d[(city, state)].append(avg)
     
     # Filter out counties with less than 20 users.
     d2 = {key: val for key, val in d.iteritems() if len(val) >= 20}
@@ -70,7 +71,7 @@ def anova():
     
     for query in [query_2014, query_2009]:
         d = average_user_scores(query)
-        d2 = county_results(d)
+        d2 = city_results(d)
         print len(d2)
         
         # Run ANOVA over resulting 417 counties (for 2009 data; still running 2014 data).
@@ -90,11 +91,21 @@ if __name__ == '__main__':
     # anova()
     
     df_2014 = pd.read_sql(query_2014, get_database_connection(port = 2001))
-    df_2009 = pd.read_sql(query_2009, get_database_connection(port = 2001))
+    stats_2014 = df_2014[['city', 'state', 'user_avg_score']].groupby(['city', 'state']).agg(['mean', 'count'])
+    stats_2014_20_users = stats_2014[stats_2014['user_avg_score']['count'] >= 20]
     
-    df_2014_means = df_2014.groupby(['county', 'state']).mean()
-    df_2014_counts = df_2014.groupby(['county', 'state']).size()
-    df_2014_concat = pd.concat([df_2014_means, df_2014_counts], axis = 1, keys = ['county', 'state'])
-    print df_2014_concat.head()
-    print df_2014_concat.columns.values
-    df_2014_filtered = df_2014_concat[df_2014_concat['0'] >= 20]
+    df_2009 = pd.read_sql(query_2009, get_database_connection(port = 2001))
+    stats_2009 = df_2009[['city', 'state', 'user_avg_score']].groupby(['city', 'state']).agg(['mean', 'count'])
+    stats_2009_20_users = stats_2009[stats_2009['user_avg_score']['count'] >= 20]
+    print stats_2009_20_users
+    
+    combined = pd.merge(stats_2014_20_users, stats_2009_20_users, left_index = True, right_index = True, how = 'inner')
+    print combined
+    
+    plt.scatter(combined['user_avg_score_x']['mean'], combined['user_avg_score_y']['mean'])
+    plt.grid()
+    plt.xlabel('Average user sentiment score by city, 2014')
+    plt.ylabel('Average user sentiment score by city, 2009')
+    plt.rcParams['xtick.major.pad'] = 8
+    plt.rcParams['ytick.major.pad'] = 8
+    plt.savefig('./compare_2009_2014_city.png')
